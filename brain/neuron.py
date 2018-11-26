8be4b9b4ed2a3857e402d04de74272b816caa6cd
KimHyunsu <khszone02@gmail.com>
2018-11-26 16:06:12 +0900

    neurotransmitters

--- brain/neuron.py
+++ brain/neuron.py
@@ -1,11 +1,24 @@
+
 class Neuron(object):
-  def __init__(self, receiver: int, sender: int, length: int):
-    self.receiver = receiver
-    self.sender = sender
-    self.length = length
+    def __init__(self, receiver: int, sender: int, length: int):
+        self.receiver = receiver
+        self.sender = sender
+        self.length = length
 
-  def send_to(self, nbr):
-    nbr.receive_from(self)
+    def send_to(self, nbr):
+        nbr.receive_from(self)
 
-  def receive_from(self, nbr):
+    def receive_from(self, nbr):
+        pass
+
+
+class SensoryNeuron(Neuron):
+    pass
+
+
+class MotorNeuron(Neuron):
+    pass
+
+
+class Interneuron(Neuron):
     pass
---
+++ brain/neurotransmitter.py
@@ -0,0 +1,39 @@
+class Neurotransmitter(object):
+    def __init__(self):
+        pass
+
+
+class Acetylcholine(Neurotransmitter):
+    pass
+
+
+class GlutamicAcid(Neurotransmitter):
+    pass
+
+
+class Glycine(Neurotransmitter):
+    pass
+
+# Gamma-Aminobutyric acid
+
+
+class GABA(Neurotransmitter):
+    pass
+
+
+class Cholecystokinin(Neurotransmitter):
+    pass
+
+# Or noradrenaline
+
+
+class Norepinephrine(Neurotransmitter):
+    pass
+
+
+class Dopamine(Neurotransmitter):
+    pass
+
+
+class Serotonin(Neurotransmitter):
+    pass
