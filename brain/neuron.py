b1f017215e534b94a66d779217426ca51e17a49f
KimHyunsu <khszone02@gmail.com>
2018-11-26 15:32:27 +0900

    base structure

---
+++ brain/neuron.py
@@ -0,0 +1,11 @@
+class Neuron(object):
+  def __init__(self, receiver: int, sender: int, length: int):
+    self.receiver = receiver
+    self.sender = sender
+    self.length = length
+
+  def send_to(self, nbr):
+    nbr.receive_from(self)
+
+  def receive_from(self, nbr):
+    pass
